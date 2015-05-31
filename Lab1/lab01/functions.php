a6277885ca50ea8c477dcc4829768b1689b7cc67
Sisky <mackes13@gmail.com>
2015-05-31 17:48:01 +1200

    commit

---
+++ Lab1/lab01/functions.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>PHP Functions</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>Use of PHP built-in functions</h1>
+<?php 
+  echo "<p>Absolute value of -9 is: ", abs(-9),".</p>";
+  echo "<p>2 to the power of 5 is: ", pow(2,5),".</p>";
+?>
+</body>
+</html>
---
+++ Lab1/lab01/myphp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="Web Development :: Lab 1" />
+<meta name="keywords" content="Web,development" />
+<title>My first web page</title>
+</head>
+<body>
+<h1>Web Development 1</h1>
+<p>Can i create a .php file that do not contain any PHP scripts?</p>
+<p>Yes, but this should be avoided, so that the server does not do any unnecessry parsing and processing.</p>
+</body>
+</html>
